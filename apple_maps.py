262c25e2fdca92ed778b0050a883d9a1981e6ed2
Saquib Khan <saquibkhan@saquibs-mbp.wireless.rit.edu>
2023-06-05 10:15:23 -0400

    added apple maps dependencies

---
+++ apple_maps.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup
+import requests
+import psycopg2
+from config.urls import APPLE_MAPS_URL
+import re
+from utils.database import Database
+
+def scrape_dependencies():
+
+    db = Database(config_file='config/db_config.json')
+    db.read_config()
+    db.connect()
+    table_name = 'apple_maps_dependencies'
+    db.create_table(table_name)
+
+    html_text = requests.get(APPLE_MAPS_URL).text
+    soup = BeautifulSoup(html_text, 'lxml')
+    elements = soup.select('p')
+    selected_elements = []
+    for element in elements:
+        next_element = element.find_next_sibling('p')
+        if next_element and 'Copyright' in next_element.text:
+            b_tag = element.find('b')
+            if b_tag:
+                selected_elements.append(b_tag.text)
+
+
+    for package in selected_elements:
+        matches = re.findall(r'\((.*?)\)', package)
+        if matches:
+            extracted_content = matches[0]
+            db.insert_dependency(extracted_content, table_name)
+
+scrape_dependencies()
